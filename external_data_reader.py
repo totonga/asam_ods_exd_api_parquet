595811d252c5350f1994d49d5e104319d2d1aea1
a.krantz <a.krantz@peak-solution.de>
2025-02-23 22:06:25 +0100

    fix for numpy 2

--- external_data_reader.py
+++ external_data_reader.py
@@ -1,19 +1,22 @@
 """EXD API implementation for parquet files"""
+
 import os
-import numpy as np
-from pathlib import Path
 import re
 import threading
-from urllib.parse import urlparse, unquote
+from pathlib import Path
+from urllib.parse import unquote, urlparse
 from urllib.request import url2pathname
 
 import grpc
-import ods_pb2 as ods
-import ods_external_data_pb2 as exd_api
-import ods_external_data_pb2_grpc
-
+import numpy as np
 import pyarrow as pa
 import pyarrow.parquet as pq
+
+import ods_external_data_pb2 as exd_api
+import ods_external_data_pb2_grpc
+import ods_pb2 as ods
+
+# pylint: disable=E1101
 
 
 class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
@@ -36,8 +39,8 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
 
         if request.suppress_channels or request.suppress_attributes or 0 != len(request.channel_names):
             context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-            context.set_details('Method not implemented!')
-            raise NotImplementedError('Method not implemented!')
+            context.set_details("Method not implemented!")
+            raise NotImplementedError("Method not implemented!")
 
         identifier = self.connection_map[request.handle.uuid]
         table = self.__get_file(request.handle)
@@ -66,14 +69,14 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
         group_id = request.group_id
         if group_id < 0 or group_id >= 1:
             context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
-            context.set_details(f'Invalid group id {request.group_id}!')
-            raise NotImplementedError(f'Invalid group id {request.group_id}!')
+            context.set_details(f"Invalid group id {request.group_id}!")
+            raise NotImplementedError(f"Invalid group id {request.group_id}!")
 
         nr_of_rows = table.num_rows
         if request.start >= nr_of_rows:
             context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
-            context.set_details(f'Channel start index {request.start} out of range!')
-            raise NotImplementedError(f'Channel start index {request.start} out of range!')
+            context.set_details(f"Channel start index {request.start} out of range!")
+            raise NotImplementedError(f"Channel start index {request.start} out of range!")
 
         end_index = request.start + request.limit
         if end_index >= nr_of_rows:
@@ -83,8 +86,8 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
         for channel_id in request.channel_ids:
             if channel_id >= table.num_columns:
                 context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
-                context.set_details(f'Invalid channel id {channel_id}!')
-                raise NotImplementedError(f'Invalid channel id {channel_id}!')
+                context.set_details(f"Invalid channel id {channel_id}!")
+                raise NotImplementedError(f"Invalid channel id {channel_id}!")
 
             channel = table.columns[channel_id]
             ods_data_type = self.__get_datatype(channel.type)
@@ -92,27 +95,37 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
             new_channel_values.id = channel_id
             new_channel_values.values.data_type = ods_data_type
             if ods.DataTypeEnum.DT_BYTE == ods_data_type:
-                new_channel_values.values.byte_array.values = np.array(channel[request.start:end_index], np.uint8).tobytes()
+                new_channel_values.values.byte_array.values = np.array(
+                    channel[request.start : end_index], np.uint8
+                ).tobytes()
             elif ods.DataTypeEnum.DT_SHORT == ods_data_type:
-                new_channel_values.values.long_array.values[:] = np.array(channel[request.start:end_index], np.int32)
+                new_channel_values.values.long_array.values[:] = np.array(channel[request.start : end_index], np.int32)
             elif ods.DataTypeEnum.DT_LONG == ods_data_type:
-                new_channel_values.values.long_array.values[:] = np.array(channel[request.start:end_index], np.int32)
+                new_channel_values.values.long_array.values[:] = np.array(channel[request.start : end_index], np.int32)
             elif ods.DataTypeEnum.DT_LONGLONG == ods_data_type:
-                new_channel_values.values.longlong_array.values[:] = np.array(channel[request.start:end_index], np.int64)
+                new_channel_values.values.longlong_array.values[:] = np.array(
+                    channel[request.start : end_index], np.int64
+                )
             elif ods.DataTypeEnum.DT_FLOAT == ods_data_type:
-                new_channel_values.values.float_array.values[:] = np.array(channel[request.start:end_index], np.float32)
+                new_channel_values.values.float_array.values[:] = np.array(
+                    channel[request.start : end_index], np.float32
+                )
             elif ods.DataTypeEnum.DT_DOUBLE == ods_data_type:
-                new_channel_values.values.double_array.values[:] = np.array(channel[request.start:end_index], np.float64)
+                new_channel_values.values.double_array.values[:] = np.array(
+                    channel[request.start : end_index], np.float64
+                )
             elif ods.DataTypeEnum.DT_DATE == ods_data_type:
-                datetime_values = channel[request.start:end_index]
+                datetime_values = channel[request.start : end_index]
                 string_values = []
                 for datetime_value in datetime_values:
                     string_values.append(self.__to_asam_ods_time(datetime_value))
                 new_channel_values.values.string_array.values[:] = string_values
             elif ods.DataTypeEnum.DT_STRING == ods_data_type:
-                new_channel_values.values.string_array.values[:] = np.array(channel[request.start:end_index], np.string_)
+                new_channel_values.values.string_array.values[:] = np.array(
+                    channel[request.start : end_index], np.bytes_
+                )
             else:
-                raise NotImplementedError(f'Not implemented channel type {ods_data_type}!')
+                raise NotImplementedError(f"Not implemented channel type {ods_data_type}!")
 
             rv.channels.append(new_channel_values)
 
@@ -120,8 +133,8 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
 
     def GetValuesEx(self, request, context):
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('Method not implemented!')
-        raise NotImplementedError('Method not implemented!')
+        context.set_details("Method not implemented!")
+        raise NotImplementedError("Method not implemented!")
 
     def __to_asam_ods_time(self, datetime_value):
         return re.sub("[^0-9]", "", str(datetime_value))
@@ -143,7 +156,7 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
             return ods.DataTypeEnum.DT_LONGLONG
         elif pa.uint64() == data_type:
             return ods.DataTypeEnum.DT_DOUBLE
-        elif pa.timestamp('us') == data_type:
+        elif pa.timestamp("us") == data_type:
             return ods.DataTypeEnum.DT_DATE
         elif pa.float32() == data_type:
             return ods.DataTypeEnum.DT_FLOAT
@@ -151,7 +164,7 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
             return ods.DataTypeEnum.DT_DOUBLE
         elif pa.string() == data_type:
             return ods.DataTypeEnum.DT_STRING
-        raise NotImplementedError(f'Unknown type {data_type}!')
+        raise NotImplementedError(f"Unknown type {data_type}!")
 
     def __init__(self):
         self.connect_count = 0
@@ -168,9 +181,7 @@ class ExternalDataReader(ods_external_data_pb2_grpc.ExternalDataReader):
     def __uri_to_path(self, uri):
         parsed = urlparse(uri)
         host = "{0}{0}{mnt}{0}".format(os.path.sep, mnt=parsed.netloc)
-        return os.path.normpath(
-            os.path.join(host, url2pathname(unquote(parsed.path)))
-        )
+        return os.path.normpath(os.path.join(host, url2pathname(unquote(parsed.path))))
 
     def __get_path(self, file_url):
         final_path = self.__uri_to_path(file_url)
